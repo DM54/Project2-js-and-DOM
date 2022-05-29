6f72b5287a1f3f0ac773cdad6483959329f7dcba
Dyana Monroy <dyana.monroy17@live.com>
2022-05-29 19:29:36 -0400

    made some process on nav and section

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,16 +1,16 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
@@ -20,14 +20,14 @@
 
 /**
  * Define Global Variables
- * 
+ *
 */
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -35,10 +35,29 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
+
+/**
+ * I am creating li for unordered list and using for loop to create 4 li with text inside.
+ *
+ */
+ const navbarlist = document.querySelector('.navbar__menu #navbar__list');
+
+ const list = () => {
+   for (let i = 1; i<=4; i++){
+     const navlist = document.createElement('li');
+     navlist.innerHTML = `Section ${i}`;
+
+    navbarlist.append(navlist);
+  }
+  navbarlist.style = "color: black";
+   //document.body.append(list);
+};
+list();
+
 
 
 // Add class 'active' to section when near top of viewport
@@ -50,13 +69,25 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
+const maintag = document.querySelector('main');
+const createnewsection = document.createElement('section');
+createnewsection.setAttribute('id', 'section4');
+createnewsection.setAttribute('data-nav', 'section 4');
+maintag.appendChild(createnewsection);
+const innercontent = document.querySelector('.landing__container');
+const clone = innercontent.cloneNode(true);
+//console.log(clone);
+createnewsection.append(clone);
+
+
 
 // Scroll to section on link click
 
 // Set sections as active
+
 
 
