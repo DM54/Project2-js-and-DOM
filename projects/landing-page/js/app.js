8eb532f45bf0d7a49da081e8bfe322a9d9df6efb
Dyana Monroy <dyana.monroy17@live.com>
2022-05-30 15:30:58 -0400

    made some fixed in the nav menu

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -41,20 +41,25 @@
 // build the nav
 
 /**
- * I am creating li for unordered list and using for loop to create 4 li with text inside.
+ * First look for navbar__menu class and navbar__list id, made arrow function called list
+ * to create li for unordered list and use for loop to create 4 li with anchor link inside. Then add them
+ * to the  navbar__menu class and navbar__list id. Also, set attribute called menu_link from the
+ * stylesheet.
  *
  */
  const navbarlist = document.querySelector('.navbar__menu #navbar__list');
+ const getnav = document.querySelector('nav');
+getnav.setAttribute("class", "navbar__menu menu__link");
 
  const list = () => {
+
    for (let i = 1; i<=4; i++){
      const navlist = document.createElement('li');
-     navlist.innerHTML = `Section ${i}`;
-
+     navlist.innerHTML += `<a href="#section${i}">Section ${i}</a>`;
+     //navlist.setAttribute('class', 'menu__link');
     navbarlist.append(navlist);
   }
-  navbarlist.style = "color: black";
-   //document.body.append(list);
+
 };
 list();
 
@@ -73,6 +78,13 @@ list();
 */
 
 // Build menu
+/**
+ * created another section in the main which is section 4,
+ * first created another section and set the attributes for the section tag then added to the main tag.
+ * I use cloneNode to the clone everything from the landing__container class and its descendants then
+ * add to the section tag that I created.
+ *
+ */
 const maintag = document.querySelector('main');
 const createnewsection = document.createElement('section');
 createnewsection.setAttribute('id', 'section4');
